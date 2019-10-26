3572060000a394bb589b4cb6622a6d57bc0f5455
Luis Rei <me@luisrei.com>
2019-10-26 22:21:08 +0200

    index fix for rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,6 +36,7 @@ extensions = [
     'sphinx.ext.napoleon',
 ]
 
+master_doc = 'index'
 autoapi_dirs = ['../../transformertc']
 
 # Add any paths that contain templates here, relative to this directory.
